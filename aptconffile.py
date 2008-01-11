5d283bb5e2267f82a2e6ee184c27c573f0bc3ea1
Alexandre Fayolle <alexandre.fayolle@logilab.fr>
2008-01-11 15:25:32 +0100

    bug fixes started working on distribution name support

--- aptconffile.py
+++ aptconffile.py
@@ -23,54 +23,42 @@ def writeconf(dest, group, perms):
     fdesc.write(APTDEFAULT_APTCONF)
     fdesc.close()
     sht.set_permissions(dest, -1, group, perms)
+    
 
 APTDEFAULT_APTCONF = '''\
-// This config is for use with the pool-structure for the packages, thus we
-// don't use a Tree Section in here
+APT {
+  FTPArchive {
+    Release {
+        Origin "WRITEME";
+        Label  "WRITEME";
+        Suite  "sid";
+        Description "WRITEME";
+    };
+  };     
+};
 
-// The debian archive should be in the current working dir
+Default {
+        Packages::Compress ". gzip bzip2";
+        Sources::Compress ". gzip bzip2";
+        Contents::Compress ". gzip bzip2";
+        FileMode 0664;
+};
+
+
 Dir {
-	ArchiveDir ".";
-	CacheDir ".";
+        ArchiveDir ".";
 };
 
-Default {
-	Packages::Compress ". gzip bzip2";
-	Sources::Compress ". gzip bzip2";
-	Contents::Compress ". gzip bzip2";
+TreeDefault {
+    Directory "debian/";
+    SrcDirectory "debian/";
+
 };
 
-// Includes the main section. You can structure the directory tree under
-// ./pool/main any way you like, apt-ftparchive will take any deb (and
-// source package) it can find. This creates a Packages a Sources and a
-// Contents file for these in the main section of the sid release
-BinDirectory "pool/main" {
-	Packages "dists/sid/main/binary-i386/Packages";
-	Sources "dists/sid/main/source/Sources";
-	Contents "dists/sid/Contents-i386";
-}
-
-// This is the same for the contrib section
-BinDirectory "pool/contrib" {
-	Packages "dists/sid/contrib/binary-i386/Packages";
-	Sources "dists/sid/contrib/source/Sources";
-	Contents "dists/sid/Contents-i386";
-}
-
-// This is the same for the non-free section
-BinDirectory "pool/non-free" {
-	Packages "dists/sid/non-free/binary-i386/Packages";
-	Sources "dists/sid/non-free/source/Sources";
-	Contents "dists/sid/Contents-i386";
+BinDirectory "debian" {
+    Packages "debian/Packages";
+    Sources "debian/Sources";
+    Contents "debian/Contents"
 };
-
-// By default all Packages should have the extension ".deb"
-Default {
-	Packages {
-		Extensions ".deb";
-	};
-};
-
-
 '''
 
