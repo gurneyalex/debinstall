3f89f8527d8f7bab10b082d16b250c5868612893
Julien Jehannet <julien.jehannet@logilab.fr>
2008-11-12 14:05:59 +0100

    clean some debian rules + remove linda in test config

--- checkers.py
+++ checkers.py
@@ -14,7 +14,7 @@
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-"""common interface to linda and lintian"""
+"""common interface to lintian"""
 
 from subprocess import Popen, PIPE
 import os
