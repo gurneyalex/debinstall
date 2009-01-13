f3c8bc8fd0bec4e17be8801a710a3889a30efd77
Julien Jehannet <julien.jehannet@logilab.fr>
2009-01-13 18:13:56 +0100

    prepare version 2.2.0

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -18,7 +18,7 @@ http://www.logilab.fr/ -- mailto:contact@logilab.fr
 
 import glob
 modname = 'debinstall'
-numversion = (2, 1, 3)
+numversion = (2, 2, 0)
 version = '.'.join([str(num) for num in numversion])
 
 
