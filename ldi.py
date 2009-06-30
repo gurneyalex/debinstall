2fcf93278da8690f440aa87c94c2e6a890c7b3ac
Julien Jehannet <julien.jehannet@logilab.fr>
2009-06-30 12:51:33 +0200

    clean bloated _filter_incoming_changes()

--- ldi.py
+++ ldi.py
@@ -258,33 +258,16 @@ class Publish(Upload):
                 if os.path.islink(osp.join(root, d)):
                     dirs.remove(d)
             for f in files:
-                if f.endswith('.changes'):
+                if f.endswith('.changes') and self._filter_incoming_changes(f):
                     changes.append(osp.join(root, f))
-        self.logger.debug("get incoming changes: %s" % changes)
-        changes = self._filter_incoming_changes(changes)
-        self.logger.debug("filtered incoming changes: %s" % changes)
         return changes
 
     def _filter_incoming_changes(self, changes):
         if self.args[1:]:
-            changes2 = []
             for f in self.args[1:]:
-                self.logger.debug("filter incoming changes: %s" % f)
-                f = osp.abspath(f)
-                self.logger.debug("filter incoming changes: %s" % f)
-                path = osp.dirname(f)
-                self.logger.debug("filter incoming changes: %s (%s)" % (f,path))
-                if os.path.islink(path):
-                    path = osp.join(os.path.dirname(path), os.readlink(path))
-                self.logger.debug("filter incoming changes: %s (%s)" % (f,path))
-                f = os.path.join(path, osp.basename(f))
-                self.logger.debug("filter incoming changes: %s" % f)
-                self._check_changes_file(f)
-                if f in changes:
-                    changes2.append(f)
-                    self.logger.debug("queue new changes file: %s" % changes2)
-            changes = changes2
-        return changes
+                return osp.basename(changes) in [osp.basename(f) for f
+                                                 in self.args[1:]]
+        return True
 
     def _run_checkers(self, changes_file):
         checkers = self.get_config_value('checkers').split()
