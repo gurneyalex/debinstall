5cf434692e0a7adb557f23e91f4f81dfef8d66e1
Julien Cristau <julien.cristau@logilab.fr>
2011-08-01 15:04:31 +0200

    python-debian renamed the 'debian_bundle' package to 'debian', follow suit
    Gets rid of this warning:
    debinstall/debfiles.py:21: DeprecationWarning: please use 'debian' instead of 'debian_bundle'
    
    Keep the old code around in case there are still users of the old (pre-squeeze) version.

--- debfiles.py
+++ debfiles.py
@@ -18,7 +18,10 @@
 import os.path as osp
 from subprocess import Popen, PIPE
 
-from debian_bundle import deb822
+try:
+    from debian import deb822
+except ImportError:
+    from debian_bundle import deb822
 
 from logilab.common.decorators import cached
 
